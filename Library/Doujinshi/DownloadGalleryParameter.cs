e5d4e5836b1b516df136b049bbdbbde361556a49
gisellevonbingen <44163945+gisellevonbingen@users.noreply.github.com>
2019-04-23 21:06:39 +0900

    클래스 빠진거 다시 추가

---
+++ Library/Doujinshi/DownloadGalleryParameter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Giselle.DoujinshiDownloader.Doujinshi
+{
+    public class DownloadGalleryParameter
+    {
+        public string Referer { get; set; }
+
+        public DownloadGalleryParameter()
+        {
+
+        }
+
+    }
+
+}
