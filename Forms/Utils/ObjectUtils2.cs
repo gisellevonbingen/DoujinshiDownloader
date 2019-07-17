0fff0d6e8ca6a7a437dcf3d4ddcf8caedfdf2e27
gisellevonbingen <44163945+gisellevonbingen@users.noreply.github.com>
2019-07-17 17:02:07 +0900

    ObjectUtils2.Execute 추가

---
+++ Forms/Utils/ObjectUtils2.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Giselle.DoujinshiDownloader.Utils
+{
+    public static class ObjectUtils2
+    {
+        public static void Execute<T>(this T obj, Action<T> action)
+        {
+            if (obj != null)
+            {
+                action(obj);
+            }
+
+        }
+
+        public static R Execute<T, R>(this T obj, Func<T, R> func)
+        {
+            if (obj != null)
+            {
+                return func(obj);
+            }
+
+            return default;
+        }
+
+    }
+
+}
