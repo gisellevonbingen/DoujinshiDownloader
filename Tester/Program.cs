3bb5ab493f0cc3c6f05cd8e5fe5c4cd02690173f
gisellevonbingen <gisellevonbingen@gmail.com>
2021-02-15 01:47:33 +0900

    갤러리 다운로드 자동 테스트
    각 케이스 별 갤러리 등록
    순차적으로 다운로드하여 이상 없는지 확인

--- Tester/Program.cs
+++ Tester/Program.cs
@@ -1,21 +1,81 @@
-﻿using Giselle.DoujinshiDownloader.Doujinshi;
+﻿using Giselle.DoujinshiDownloader;
+using Giselle.DoujinshiDownloader.Doujinshi;
+using Giselle.DoujinshiDownloader.Schedulers;
+using Giselle.DoujinshiDownloader.Utils;
 using System;
 using System.Collections.Generic;
-using System.Diagnostics;
-using System.Drawing;
 using System.Linq;
 using System.Text;
-using System.Text.RegularExpressions;
 using System.Threading;
 using System.Threading.Tasks;
-using System.Windows.Forms;
 
 namespace Tester
 {
     public class Program
     {
         [STAThread]
-        public static void Main(string[] args)
+        public static void Main()
+        {
+            using (var instance = new DoujinshiDownloader(new CommandLineOptions()))
+            {
+                TestDownloadInputParse();
+                TestDownload();
+            }
+
+        }
+
+        public static void TestDownload()
+        {
+            var tests = new List<DownloadTest>();
+            tests.Add(new DownloadTest(new DownloadInput(1651862), DownloadMethod.Hitomi)); //WEBP, AVIF
+            tests.Add(new DownloadTest(new DownloadInput(1846070), DownloadMethod.Hitomi)); //WEBP, AVIF
+            tests.Add(new DownloadTest(new DownloadInput(1474824), DownloadMethod.Hitomi)); //JPG
+            tests.Add(new DownloadTest(new DownloadInput(1490664), DownloadMethod.Hitomi)); //JPG
+            tests.Add(new DownloadTest(new DownloadInput(1836951), DownloadMethod.Hitomi)); //GIF
+
+            foreach (var test in tests)
+            {
+                var input = test.Input;
+                var site = test.Method.Site;
+                var agent = test.Method.CreateAgent();
+                var info = agent.GetGalleryInfo(site, input);
+                var thumbnail = agent.GetGalleryThumbnail(info.ThumbnailUrl);
+
+                var request = new DownloadRequest();
+                request.Validation = GalleryValidation.CreateByInfo(site, input, agent, info, thumbnail);
+                request.FileName = PathUtils.FilterInvalids(info.Title);
+
+                var printed = false;
+
+                using (var task = new DownloadTask(request))
+                {
+                    task.Start();
+
+                    while (true)
+                    {
+                        if (task.Count > 0 && printed == false)
+                        {
+                            printed = true;
+                            Console.WriteLine(string.Join(Environment.NewLine, task.ImageViewStates.Select(s => s.View.Url)));
+                        }
+
+                        Console.WriteLine($"{input} : {task.Index}/{task.Count}, {task.Index / (task.Count / 100.0D):F2}");
+
+                        if (task.Running == false)
+                        {
+                            break;
+                        }
+
+                        Thread.Sleep(1000);
+                    }
+
+                }
+
+            }
+
+        }
+
+        public static void TestDownloadInputParse()
         {
             var list = new List<string>();
             list.Add("1426111");
@@ -37,7 +97,18 @@ namespace Tester
                 Console.WriteLine();
             }
 
-            return;
+        }
+
+        public class DownloadTest : Tuple<DownloadInput, DownloadMethod>
+        {
+            public DownloadInput Input => this.Item1;
+            public DownloadMethod Method => this.Item2;
+
+            public DownloadTest(DownloadInput input, DownloadMethod method) : base(input, method)
+            {
+
+            }
+
         }
 
     }
